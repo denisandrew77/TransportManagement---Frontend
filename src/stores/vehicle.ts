d305565a0ca654f6c84f89374373230a7907ccb8
DenisAndrew77 <denis77andrew@gmail.com>
2026-02-10 18:45:20 +0200

    create vehicle fetch and reset vehicle store function

--- src/stores/vehicle.ts
+++ src/stores/vehicle.ts
@@ -34,7 +34,44 @@ export const useVehicles = defineStore('vehicles', {
     vehicles: [] as vehicle[]
   }),
   actions: {
-    async createVehicle(){
+    async createVehicle(carrierName: string){
+      const token = localStorage.getItem("token");
+      await api.post("/addCarrierVehicle",{
+        vehicle: {
+          plateNumber: this.plateNumber,
+          driverName: this.driverName,
+          driverPhoneNumber: this.driverPhoneNumber,
+          status: this.status,
+          loadWeight: this.loadWeight,
+          totalLength: this.totalLength,
+          width: this.width,
+          distanceBetweenWheels: this.distanceBetweenWheels,
+          insideHeight: this.insideHeight,
+          totalHeight: this.totalHeight,
+          vehicleType: this.vehicleType,
+          carrierType: this.carrierType,
+          personalPhoneNumber: this.personalPhoneNumber,
+          goodsInsurance: this.goodsInsurance,
+          obligateInspection: this.obligateInspection,
+          weight: this.weight,
+          dkvDate: this.dkvDate,
+          gesTransDate: this.gesTransDate,
+          tarifApproachKms: this.tarifApproachKms,
+          tarrifLoadKms: this.tarrifLoadKms,
+          tarifClosingKms: this.tarifClosingKms,
+          registrationDate: this.registrationDate,
+          totalKilometers: this.totalKilometers,
+          date: this.date,
+          vehicleInsurance: this.vehicleInsurance,
+          tarif: this.tarif,
+          insideLength: this.insideLength,
+        },
+        carrierName: carrierName
+      },{
+        headers:{
+          'Authorization': token
+        }
+      });
     },
     async getCarrierVehicles(commercialName: string){
       const token = localStorage.getItem('token');
@@ -48,6 +85,35 @@ export const useVehicles = defineStore('vehicles', {
       }).then((response)=>{
         this.vehicles = response.data.vehicles;
       });
+    },
+    resetVehicleFields(){
+      this.plateNumber = '';
+      this.driverName = '';
+      this.driverPhoneNumber = '';
+      this.status = '';
+      this.loadWeight = 0;
+      this.totalLength = 0;
+      this.width = 0;
+      this.distanceBetweenWheels = 0;
+      this.insideHeight = 0;
+      this.totalHeight = 0;
+      this.vehicleType = '';
+      this.carrierType = '';
+      this.personalPhoneNumber = '';
+      this.goodsInsurance = '';
+      this.obligateInspection = '';
+      this.weight = 0;
+      this.dkvDate = '';
+      this.gesTransDate = '';
+      this.tarifApproachKms = 0;
+      this.tarrifLoadKms = 0;
+      this.tarifClosingKms = 0;
+      this.registrationDate = '';
+      this.totalKilometers = 0;
+      this.date = '';
+      this.vehicleInsurance = '';
+      this.tarif = 0;
+      this.insideLength = 0;
     }
   }
 })

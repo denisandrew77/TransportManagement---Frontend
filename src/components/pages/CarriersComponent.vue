002726aca6e06371aba9beaa1b8da71445aa3db3
DenisAndrew77 <denis77andrew@gmail.com>
2026-02-11 18:42:44 +0200

    table is standalone component

--- src/components/pages/CarriersComponent.vue
+++ src/components/pages/CarriersComponent.vue
@@ -1,24 +1,25 @@
 <script setup lang="ts">
 import { useCarrierStore } from '@/stores/carrier';
 import { useCarriers } from '@/stores/carriers';
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, type Ref } from 'vue';
 import { useRouter } from 'vue-router';
+import CompaniesTable from '../shared/Tables/CompaniesTable.vue';
+import type { carrier } from '@/models/cargo-related/carrier';
 
 const carriers = useCarriers();
 const theCarrier = useCarrierStore();
 const router = useRouter();
 const dialogVisibility = ref(false);
 const currentCarrier = ref("");
+const searchingCarrierValue = ref("");
+const carrierList: Ref<carrier[]> = ref([]);
 onMounted(async ()=>{
-  await carriers.getCarriers()
+  await carriers.getCarriers();
+  carrierList.value = carriers.carrierList;
 })
-const deleteCarrier = async ()=>{
-  carriers.carrierList.forEach(async (carrier)=>{
-    if(carrier.commercialName===currentCarrier.value){
-      await carriers.deleteCarrier(carrier.commercialName);
-      carriers.carrierList.splice(carriers.carrierList.indexOf(carrier),1);
-    }
-  })
+const deleteCarrier = async (commercialName: string)=>{
+  setCurrentCarrier(commercialName);
+  await carriers.deleteCarrier(commercialName);
   dialogVisibility.value=false;
 }
 
@@ -35,47 +36,28 @@ const editCarrier = (carrierName: string)=>{
   })
   router.push({name: 'NewCarrier', params: {state: 'edit'}});
 }
+
+const filterCompanies = () =>{
+  carrierList.value = carriers.carrierList;
+  carrierList.value=carrierList.value.filter((carrier)=>carrier.fiscalName.toLowerCase().includes(searchingCarrierValue.value.toLowerCase()))
+  if(searchingCarrierValue.value==="")
+  carrierList.value = carriers.carrierList;
+}
 </script>
 <template>
   <div class="w-full flex flex-col items-center mt-8 px-4">
     <div class="w-full">
       <div class="flex justify-between items-center mb-6 px-4">
         <h1 class="text-3xl font-bold text-gray-800">Carriers</h1>
-        <el-button type="primary" size="large" @click="router.push('/newCarrier')">
-          <i class="bi bi-plus-lg mr-2"></i>Add Carrier
-        </el-button>
+        <div class="flex flex-row items-center gap-4">
+          <span>Search</span>
+          <el-input v-model="searchingCarrierValue" @input="filterCompanies"/>
+          <el-button type="primary" size="large" @click="router.push('/newCarrier')">
+            <i class="bi bi-plus-lg mr-2"></i>Add Carrier
+          </el-button>
+        </div>
       </div>
-      <el-table
-        :data="carriers.carrierList"
-        border
-        stripe
-        class="w-full shadow-lg"
-        :header-cell-style="{ background: '#1e40af', color: '#fff', fontWeight: 'bold' }"
-      >
-        <el-table-column prop="fiscalCode" label="NIF" min-width="140" fixed/>
-        <el-table-column prop="commercialName" label="Commercial Name" min-width="200" fixed/>
-        <el-table-column prop="fiscalName" label="Fiscal Name" min-width="200"/>
-        <el-table-column prop="country" label="Country" min-width="120"/>
-        <el-table-column prop="city" label="City" min-width="150"/>
-        <el-table-column prop="postalCode" label="Postal Code" min-width="130"/>
-        <el-table-column prop="currency" label="Currency" min-width="100"/>
-        <el-table-column label="Actions" width="180" fixed="right" align="center">
-          <template #default="scope">
-            <el-button type="primary" size="small" @click="editCarrier(scope.row.commercialName)">
-              <div class="flex flex-row gap-1">
-                <i class="bi bi-pencil"></i>
-                <div>Edit</div>
-              </div>
-            </el-button>
-            <el-button type="danger" size="small" @click="setCurrentCarrier(scope.row.commercialName)">
-              <div class="flex flex-row gap-1">
-                <i class="bi bi-trash"></i>
-                <div>Delete</div>
-              </div>
-            </el-button>
-          </template>
-        </el-table-column>
-      </el-table>
+      <CompaniesTable :carriers="carrierList" @editRequest="editCarrier" @deleteRequest="setCurrentCarrier"/>
     </div>
   </div>
   <el-dialog v-model="dialogVisibility" width="500">
@@ -89,7 +71,7 @@ const editCarrier = (carrierName: string)=>{
     <p class="text-gray-700">Are you sure you want to delete {{currentCarrier}}?</p>
     <div class="flex flex-row gap-4">
       <el-button @click="dialogVisibility = false" type="primary">Cancel</el-button>
-      <el-button @click="async ()=>await deleteCarrier()" type="danger">Delete</el-button>
+      <el-button @click="async ()=>await deleteCarrier(currentCarrier)" type="danger">Delete</el-button>
     </div>
   </div>
   </el-dialog>
---
+++ src/components/shared/Tables/CompaniesTable.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import type { carrier } from '@/models/cargo-related/carrier';
+
+defineProps<{
+  carriers: Array<carrier>
+}>();
+
+const emit = defineEmits<{
+  (e:"editRequest", value: string):void
+  (e:"deleteRequest", value: string):void
+}>()
+</script>
+<template>
+<el-table
+:data="carriers"
+border
+stripe
+class="w-full shadow-lg"
+:header-cell-style="{ background: '#1e40af', color: '#fff', fontWeight: 'bold' }"
+>
+  <el-table-column prop="fiscalCode" label="NIF" min-width="140" fixed sortable/>
+  <el-table-column prop="address" label="Address" min-width="200" fixed/>
+  <el-table-column prop="fiscalName" label="Fiscal Name" min-width="200" sortable/>
+  <el-table-column prop="country" label="Country" min-width="120" sortable/>
+  <el-table-column prop="city" label="City" min-width="150" sortable/>
+  <el-table-column prop="postalCode" label="Postal Code" min-width="130" sortable/>
+  <el-table-column prop="currency" label="Currency" min-width="100" sortable/>
+  <el-table-column label="Actions" width="180" fixed="right" align="center">
+    <template #default="scope">
+      <el-button type="primary" size="small" @click="emit('editRequest', scope.row.commercialName)">
+        <div class="flex flex-row gap-1">
+          <i class="bi bi-pencil"></i>
+          <div>Edit</div>
+        </div>
+      </el-button>
+      <el-button type="danger" size="small" @click="emit('deleteRequest', scope.row.commercialName)">
+        <div class="flex flex-row gap-1">
+          <i class="bi bi-trash"></i>
+          <div>Delete</div>
+        </div>
+      </el-button>
+    </template>
+  </el-table-column>
+</el-table>
+</template>

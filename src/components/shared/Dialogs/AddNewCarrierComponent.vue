9a80ddbe709f94028a73549001332eb51a34007c
DenisAndrew77 <denis77andrew@gmail.com>
2026-02-11 18:59:06 +0200

    value binding fixed

--- src/components/shared/Dialogs/AddNewCarrierComponent.vue
+++ src/components/shared/Dialogs/AddNewCarrierComponent.vue
@@ -36,7 +36,7 @@ const inputTitleList = ref({
 </script>
 <template>
   <div class="mt-3 grid grid-cols-4 gap-4 relative mb-5">
-    <InputWithText :label="inputTitleList.nif" v-model="carrier.nif" />
+    <InputWithText :label="inputTitleList.nif" v-model="carrier.fiscalCode" />
     <InputWithText :label="inputTitleList.fiscalName" v-model="carrier.fiscalName" />
     <InputWithText :label="inputTitleList.orc" v-model="carrier.orc" />
     <InputWithText :label="inputTitleList.commercialName" v-model="carrier.commercialName" />
@@ -49,8 +49,8 @@ const inputTitleList = ref({
     <InputWithText :label="inputTitleList.address" v-model="carrier.address" />
     <InputWithText :label="inputTitleList.city" v-model="carrier.city" />
     <InputWithText :label="inputTitleList.swift" v-model="carrier.swift" />
-    <InputWithText :label="inputTitleList.phone" v-model="carrier.phone" />
-    <InputWithText :label="inputTitleList.bank" v-model="carrier.bank" />
+    <InputWithText :label="inputTitleList.phone" v-model="carrier.phoneNumber" />
+    <InputWithText :label="inputTitleList.bank" v-model="carrier.bankName" />
     <InputWithText :label="inputTitleList.iban" v-model="carrier.iban" />
     <NumberInputWithText :label="inputTitleList.vatPercentage" v-model="carrier.vatPercentage" />
       <el-button @click="carrier.createCarrier" type="success" class="absolute right-8 bottom-0" size="large">Save</el-button>

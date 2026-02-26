e619f18a1c475d4d958d2ede446c471e4a41c240
DenisAndrew77 <denis77andrew@gmail.com>
2026-02-26 10:20:07 +0200

    time input and country selector adjusted

---
+++ src/components/shared/Inputs/CountrySelector.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { countries } from 'countries-list'
+defineProps<{
+  modelValue: string
+}>()
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string): void
+}>()
+
+const handleChange = (value: string) => {
+  emit('update:modelValue', value)
+}
+</script>
+<template>
+    <el-select filterable :model-value="modelValue" @update:model-value="handleChange">
+      <el-option v-for="(country, code) in countries" :key="code" :label="country.name" :value="code" />
+    </el-select>
+</template>

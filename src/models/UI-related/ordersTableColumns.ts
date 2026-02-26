dddfa670a288107b0c5ba7a1e48d513a323de681
DenisAndrew77 <denis77andrew@gmail.com>
2026-02-26 10:20:52 +0200

    ui order table interface

---
+++ src/models/UI-related/ordersTableColumns.ts
@@ -0,0 +1,11 @@
+export interface orderTableColumn{
+  orderNumber: number,
+  plateNumber: string[],
+  carrier: string[],
+  clientNumber: string,
+  client: string,
+  loading: string[],
+  delivery: string[],
+  status: string,
+  info: string[],
+}

21ee9cfd721f9710552c0afa51a9ea2604ee3875
DenisAndrew77 <denis77andrew@gmail.com>
2026-03-09 18:26:11 +0200

    flag icons

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import "../src/assets/main.css";
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'flag-icons/css/flag-icons.min.css';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';

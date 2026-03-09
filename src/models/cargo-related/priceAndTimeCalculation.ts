834e525ae84196dc8efccd1f8bae98af2d608f45
DenisAndrew77 <denis77andrew@gmail.com>
2026-03-09 18:28:57 +0200

    data adjustment to price calculation

--- src/models/cargo-related/priceAndTimeCalculation.ts
+++ src/models/cargo-related/priceAndTimeCalculation.ts
@@ -1,4 +1,15 @@
-export interface routeAndPriceDetails {
+export interface individualCarrierPrice {
+highways: number
+waiting: number
+hotel: number
+carrierPrice: number
+totalCarrierPrice: number
+}
+export interface finalPrice{
+  totalCarriersPrice: number
   clientPrice: number
-  carrierPrice: number
+}
+
+export interface routeAndPriceDetails extends individualCarrierPrice, finalPrice{
+
 }

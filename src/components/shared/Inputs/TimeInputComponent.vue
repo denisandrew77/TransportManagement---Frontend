4fd852db0219c8aaed1281aab9244c8a654e224b
DenisAndrew77 <denis77andrew@gmail.com>
2026-05-11 13:46:12 +0300

    Timeinput width change

--- src/components/shared/Inputs/TimeInputComponent.vue
+++ src/components/shared/Inputs/TimeInputComponent.vue
@@ -9,7 +9,7 @@ const emit = defineEmits<{
 <template>
   <div class="flex flex-row">
     <input :value="modelValue" @input="emit('update:modelValue', ($event.target as HTMLInputElement).value)" type="text"
-      class="bg-white rounded-l-sm px-2 py-1 focus:outline-none focus:ring-0 w-17" />
+      class="bg-white rounded-l-sm px-2 py-1 focus:outline-none focus:ring-0 w-24" />
     <div class="flex items-center bg-white rounded-r-sm pr-2">
       <i class="bi bi-clock"></i>
     </div>

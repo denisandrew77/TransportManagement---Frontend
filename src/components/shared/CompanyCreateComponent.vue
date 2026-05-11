dee0a0a07a4f28ba87a63035e7c2adf833adc814
DenisAndrew77 <denis77andrew@gmail.com>
2026-05-11 13:43:57 +0300

    changed design in CreateCompany component

--- src/components/shared/CompanyCreateComponent.vue
+++ src/components/shared/CompanyCreateComponent.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import InputWithText from './Inputs/InputWithText.vue';
-import CountrySelectorWithLabel from './Inputs/CountrySelectorWithLabel.vue';
-import CurrencySelectorWithLabel from './Inputs/CurrencySelectorWithLabel.vue';
-import DatePickerComponent from './Inputs/DatePickerComponent.vue';
-import NumberInputWithText from './Inputs/NumberInputWithText.vue';
+import { countries } from 'countries-list';
+import cc from 'currency-codes';
 import type { carrier } from '@/models/cargo-related/carrier';
 import type { carrierTitles } from '@/models/UI-related/carrierTitles';
+
+const currencyList = cc.data.map((c) => ({ code: c.code, name: c.currency }))
 const props = defineProps<{
   inputTitleList: carrierTitles
   modelValue: carrier
@@ -32,27 +31,93 @@ const validateCodeRequest = () => {
 }
 </script>
 <template>
-<div class="w-full flex justify-center">
-    <div class="mt-3 grid grid-cols-4 place-items-center gap-4 relative mb-5 w-2/4 border border-black rounded-xl p-5 shadow-2xl">
-    <InputWithText :label="inputTitleList.nif" :modelValue="modelValue.fiscalCode" @update:modelValue="updateField('fiscalCode', $event)"/>
-    <InputWithText :label="inputTitleList.fiscalName" :modelValue="modelValue.fiscalName" @update:modelValue="updateField('fiscalName', $event)"/>
-    <InputWithText :label="inputTitleList.orc" :modelValue="modelValue.orc" @update:modelValue="updateField('orc', $event)"/>
-    <InputWithText :label="inputTitleList.commercialName" :modelValue="modelValue.commercialName" @update:modelValue="updateField('commercialName', $event)"/>
-    <CountrySelectorWithLabel :label="inputTitleList.country" class="w-48" :modelValue="modelValue.country" @update:modelValue="updateField('country', $event)"/>
-    <CurrencySelectorWithLabel :label="inputTitleList.currency" class="w-48" :modelValue="modelValue.currency" @update:modelValue="updateField('currency', $event)"/>
-    <InputWithText :label="inputTitleList.capital" :modelValue="modelValue.capital" @update:modelValue="updateField('capital', $event)"/>
-    <DatePickerComponent :label="inputTitleList.registered" :modelValue="modelValue.registered" @update:modelValue="updateField('registered', $event)"/>
-    <InputWithText :label="inputTitleList.postalCode" :modelValue="modelValue.postalCode" @update:modelValue="updateField('postalCode', $event)"/>
-    <InputWithText :label="inputTitleList.county" :modelValue="modelValue.county" @update:modelValue="updateField('county', $event)"/>
-    <InputWithText :label="inputTitleList.address" :modelValue="modelValue.address" @update:modelValue="updateField('address', $event)"/>
-    <InputWithText :label="inputTitleList.city" :modelValue="modelValue.city" @update:modelValue="updateField('city', $event)"/>
-    <InputWithText :label="inputTitleList.swift" :modelValue="modelValue.swift" @update:modelValue="updateField('swift', $event)"/>
-    <InputWithText :label="inputTitleList.phone" :modelValue="modelValue.phoneNumber" @update:modelValue="updateField('phoneNumber', $event)"/>
-    <InputWithText :label="inputTitleList.bank" :modelValue="modelValue.bankName" @update:modelValue="updateField('bankName', $event)"/>
-    <InputWithText :label="inputTitleList.iban" :modelValue="modelValue.iban" @update:modelValue="updateField('iban', $event)"/>
-    <NumberInputWithText :label="inputTitleList.vatPercentage" :modelValue="modelValue.vatPercentage" @update:modelValue="updateField('vatPercentage', $event)"/>
-      <el-button @click="saveRequest" type="success" class="absolute right-4 bottom-4" size="large">Save</el-button>
-      <el-button @click="validateCodeRequest" type="primary" class="absolute right-30 bottom-4" size="large">Validate VAT code</el-button>
+  <div class="w-full flex justify-center py-8">
+<div class="w-2/4 bg-slate-50 rounded-2xl shadow-lg p-8 flex flex-col gap-10">
+
+      <div class="grid grid-cols-4 gap-x-6 gap-y-8">
+        <div class="flex flex-col gap-1">
+          <span class="text-sm text-gray-500">{{ inputTitleList.nif }}</span>
+          <el-input :model-value="modelValue.fiscalCode" @update:model-value="updateField('fiscalCode', $event)"/>
+        </div>
+        <div class="flex flex-col gap-1">
+          <span class="text-sm text-gray-500">{{ inputTitleList.fiscalName }}</span>
+          <el-input :model-value="modelValue.fiscalName" @update:model-value="updateField('fiscalName', $event)"/>
+        </div>
+        <div class="flex flex-col gap-1">
+          <span class="text-sm text-gray-500">{{ inputTitleList.orc }}</span>
+          <el-input :model-value="modelValue.orc" @update:model-value="updateField('orc', $event)"/>
+        </div>
+        <div class="flex flex-col gap-1">
+          <span class="text-sm text-gray-500">{{ inputTitleList.commercialName }}</span>
+          <el-input :model-value="modelValue.commercialName" @update:model-value="updateField('commercialName', $event)"/>
+        </div>
+
+        <div class="flex flex-col gap-1">
+          <span class="text-sm text-gray-500">{{ inputTitleList.country }}</span>
+          <el-select filterable :model-value="modelValue.country" @update:model-value="updateField('country', $event)">
+            <el-option v-for="(country, code) in countries" :key="code" :label="country.name" :value="code" />
+          </el-select>
+        </div>
+        <div class="flex flex-col gap-1">
+          <span class="text-sm text-gray-500">{{ inputTitleList.currency }}</span>
+          <el-select filterable :model-value="modelValue.currency" @update:model-value="updateField('currency', $event)">
+            <el-option v-for="c in currencyList" :key="c.code" :label="`${c.code} - ${c.name}`" :value="c.code" />
+          </el-select>
+        </div>
+        <div class="flex flex-col gap-1">
+          <span class="text-sm text-gray-500">{{ inputTitleList.capital }}</span>
+          <el-input :model-value="modelValue.capital" @update:model-value="updateField('capital', $event)"/>
+        </div>
+        <div class="flex flex-col gap-1">
+          <span class="text-sm text-gray-500">{{ inputTitleList.registered }}</span>
+          <el-date-picker :model-value="modelValue.registered" @update:model-value="updateField('registered', $event)" type="date" placeholder="Select date" style="width: 100%" />
+        </div>
+
+        <div class="flex flex-col gap-1">
+          <span class="text-sm text-gray-500">{{ inputTitleList.postalCode }}</span>
+          <el-input :model-value="modelValue.postalCode" @update:model-value="updateField('postalCode', $event)"/>
+        </div>
+        <div class="flex flex-col gap-1">
+          <span class="text-sm text-gray-500">{{ inputTitleList.county }}</span>
+          <el-input :model-value="modelValue.county" @update:model-value="updateField('county', $event)"/>
+        </div>
+        <div class="flex flex-col gap-1">
+          <span class="text-sm text-gray-500">{{ inputTitleList.address }}</span>
+          <el-input :model-value="modelValue.address" @update:model-value="updateField('address', $event)"/>
+        </div>
+        <div class="flex flex-col gap-1">
+          <span class="text-sm text-gray-500">{{ inputTitleList.city }}</span>
+          <el-input :model-value="modelValue.city" @update:model-value="updateField('city', $event)"/>
+        </div>
+
+        <div class="flex flex-col gap-1">
+          <span class="text-sm text-gray-500">{{ inputTitleList.swift }}</span>
+          <el-input :model-value="modelValue.swift" @update:model-value="updateField('swift', $event)"/>
+        </div>
+        <div class="flex flex-col gap-1">
+          <span class="text-sm text-gray-500">{{ inputTitleList.phone }}</span>
+          <el-input :model-value="modelValue.phoneNumber" @update:model-value="updateField('phoneNumber', $event)"/>
+        </div>
+        <div class="flex flex-col gap-1">
+          <span class="text-sm text-gray-500">{{ inputTitleList.bank }}</span>
+          <el-input :model-value="modelValue.bankName" @update:model-value="updateField('bankName', $event)"/>
+        </div>
+        <div class="flex flex-col gap-1">
+          <span class="text-sm text-gray-500">{{ inputTitleList.iban }}</span>
+          <el-input :model-value="modelValue.iban" @update:model-value="updateField('iban', $event)"/>
+        </div>
+
+        <div class="flex flex-col gap-1">
+          <span class="text-sm text-gray-500">{{ inputTitleList.vatPercentage }}</span>
+          <el-input-number :model-value="modelValue.vatPercentage" @update:model-value="updateField('vatPercentage', $event)" :min="0" :max="100" controls-position="right"/>
+        </div>
+      </div>
+
+      <div class="flex justify-end gap-3 pt-4 border-t border-gray-100">
+        <el-button @click="validateCodeRequest" type="primary" size="large">Validate VAT code</el-button>
+        <el-button @click="saveRequest" type="success" size="large">Save</el-button>
+      </div>
+
     </div>
   </div>
 </template>
